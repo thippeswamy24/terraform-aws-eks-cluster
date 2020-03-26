689eee54451890dedbd15ff8c9834ff40a215906
thippeswamy24 <61265289+thippeswamy24@users.noreply.github.com>
2020-03-26 15:04:59 +0530

    Update private-route.tf

--- private-route.tf
+++ private-route.tf
@@ -2,5 +2,5 @@
 resource "aws_route" "nat_gtw" {
  route_table_id = aws_route_table.eks-private.id
  destination_cidr_block = "0.0.0.0/0"
- nat_gateway_id = aws_nat_gateway.nat_gw.*.id   
+ nat_gateway_id = aws_nat_gateway.nat_gw.id   
 }
