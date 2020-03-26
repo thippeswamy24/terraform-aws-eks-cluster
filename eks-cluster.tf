ae59d3bd563fd366a57b8f7850aa9e67cbff4f17
thippeswamy24 <61265289+thippeswamy24@users.noreply.github.com>
2020-03-26 12:28:43 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -26,7 +26,7 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.eks-cluster.name}"
+  role       = aws_iam_role.eks-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSServicePolicy" {
