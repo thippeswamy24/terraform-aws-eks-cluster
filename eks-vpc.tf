4ad8b3eb948a90593a71d300d4e6907c465806e2
thippeswamy24 <61265289+thippeswamy24@users.noreply.github.com>
2020-03-26 14:59:18 +0530

    Update eks-vpc.tf

--- eks-vpc.tf
+++ eks-vpc.tf
@@ -67,7 +67,7 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "nat_gw" {
-  #count = 1
+  count = 1
   
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.eks-public.*.id[count.index]  #public subnet 
