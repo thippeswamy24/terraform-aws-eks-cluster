cc51e1ed9e28e20721f42c9a4b60390694da2fbc
thippeswamy24 <61265289+thippeswamy24@users.noreply.github.com>
2020-03-26 12:58:19 +0530

    Update eks-vpc.tf

--- eks-vpc.tf
+++ eks-vpc.tf
@@ -14,24 +14,18 @@ resource "aws_vpc" "eks" {
 
 ## EKS public subnets
 resource "aws_subnet" "eks-public" {
-  count = "${length(var.public_subnets)}"
+  count = length(var.public_subnets)
 
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-  cidr_block        = "${var.public_subnets[count.index]}"
-  vpc_id            = "${aws_vpc.eks.id}"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = var.public_subnets[count.index]
+  vpc_id            = aws_vpc.eks.id
 
-  tags = "${
-    map(
-     "Name", "eks-public-subnet",
-     "kubernetes.io/cluster/${var.cluster-name}", "shared",
-    )
-  }"
 }
 
 
 ## internet gateway
 resource "aws_internet_gateway" "eks-igw" {
-  vpc_id = "${aws_vpc.eks.id}"
+  vpc_id = aws_vpc.eks.id
 
   tags {
     Name = "eks-internet-gateway"
@@ -39,20 +33,20 @@ resource "aws_internet_gateway" "eks-igw" {
 }
 
 resource "aws_route_table" "eks-public" {
-  vpc_id = "${aws_vpc.eks.id}"
+  vpc_id = aws_vpc.eks.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.eks-igw.id}"
+    gateway_id = aws_internet_gateway.eks-igw.id
   }
 
 }
 
 resource "aws_route_table_association" "eks" {
-  count = "${length(var.public_subnets)}"
+  count = length(var.public_subnets)
 
-  subnet_id      = "${aws_subnet.eks-public.*.id[count.index]}"
-  route_table_id = "${aws_route_table.eks-public.id}"
+  subnet_id      = aws_subnet.eks-public.*.id[count.index]
+  route_table_id = aws_route_table.eks-public.id
 }
 
 ## EKS private subnets
@@ -60,11 +54,11 @@ resource "aws_route_table_association" "eks" {
 ## routing table, routing table association
 
 resource "aws_subnet" "eks-private" {
-  count = "${length(var.private_subnets)}"
+  count = length(var.private_subnets)
 
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-  cidr_block        = "${var.private_subnets[count.index]}"
-  vpc_id            = "${aws_vpc.eks.id}"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = var.private_subnets[count.index]
+  vpc_id            = aws_vpc.eks.id
 
   tags = "${
     map(
@@ -82,8 +76,8 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "nat_gw" {
   count = 1
   
-  allocation_id = "${aws_eip.nat.id}"
-  subnet_id     = "${aws_subnet.eks-public.*.id[count.index]}"  #public subnet 
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.eks-public.*.id[count.index]}"  #public subnet 
   depends_on = ["aws_internet_gateway.eks-igw"]
 
   tags {
@@ -93,7 +87,7 @@ resource "aws_nat_gateway" "nat_gw" {
 
 
 resource "aws_route_table" "eks-private" {
-  vpc_id = "${aws_vpc.eks.id}"
+  vpc_id = aws_vpc.eks.id
 
   tags {
         Name = "route table for private subnets"
@@ -101,8 +95,8 @@ resource "aws_route_table" "eks-private" {
 }
 
 resource "aws_route_table_association" "eks-private" {
-  count = "${length(var.private_subnets)}"
+  count = length(var.private_subnets)
 
-  subnet_id      = "${aws_subnet.eks-private.*.id[count.index]}"
-  route_table_id = "${aws_route_table.eks-private.id}"
+  subnet_id      = aws_subnet.eks-private.*.id[count.index]
+  route_table_id = aws_route_table.eks-private.id
 }
